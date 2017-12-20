c41f0b043fd06675e1103761436bfc6394eaaf3d
root <damducthoai@gmail.com>
2017-12-20 12:33:37 +0000

    add shipurldata service

---
+++ src/app/shipurldata.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { ShipurldataService } from './shipurldata.service';
+
+describe('ShipurldataService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ShipurldataService]
+    });
+  });
+
+  it('should be created', inject([ShipurldataService], (service: ShipurldataService) => {
+    expect(service).toBeTruthy();
+  }));
+});
